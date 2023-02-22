9adc39c0f456ff6c0bd69c3bebfc45c6fcea64c3
salehAhmed19 <saleh.ahmed.mahin@gmail.com>
2023-02-22 23:56:54 +0600

    newsletter section responsive

--- src/Pages/Subscribe.jsx
+++ src/Pages/Subscribe.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import img from "../Assets/Img/img-newsletter.png";
 import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
 import Pulse from "react-reveal/Pulse";
+import stat from "../Assets/Img/chart.png";
 
 const Subscribe = () => {
   return (
-    <div className="lg:w-3/4 bg-[#023A20] py-14 lg:px-10 px-3 mx-4 lg:mx-auto mb-36 mt-10 flex flex-col lg:flex-row justify-between items-center">
+    <div className="lg:w-4/5 bg-[#023A20] lg:py-14 py-5 lg:px-10 px-3 mx-4 lg:mx-auto mb-36 mt-10 flex flex-col lg:flex-row justify-between items-center rounded-lg">
       <div>
         <h4 className="text-[#6BF176] text-sm lg:text-base">NEWSLETTER</h4>
         <h2 className="text-xl lg:text-5xl font-bold text-[#fff] mt-3">
@@ -25,14 +26,20 @@ const Subscribe = () => {
           </button>
         </div>
       </div>
-      <div>
+
+      <div className="flex flex-col-reverse lg:flex-row">
         <Pulse forever={true}>
           <img
-            className="w-72 rounded-lg mb-[-150px] mt-7 mx-auto"
-            src={img}
+            className="w-56 h-44 rounded-lg mr-[-40px] lg:mt-[66px] mt-[-150px] z-10"
+            src={stat}
             alt=""
           />
         </Pulse>
+        <img
+          className="lg:w-72 w-full rounded-lg lg:mb-[-100px] mt-5 lg:mt-0 mx-auto"
+          src={img}
+          alt=""
+        />
       </div>
     </div>
   );
